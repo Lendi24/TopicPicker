d2b1ca2f2b8a3a3b2e4c3e8996092deae6edf61e
Lendi24 <Lendic24@hotmail.com>
2023-01-12 13:18:03 +0100

    hoof

--- scripts/SPA-Tools/loader.js
+++ scripts/SPA-Tools/loader.js
@@ -10,16 +10,31 @@ class HtmlLoader {
         let inputLabel1 = "Error!";
         let inputLabel2 = "Error!";
         let inputLabel3 = "Error!";
-        switch (urlArgs.editMode) {
+        let inputPrefill1 = "";
+        let inputPrefill2 = "";
+        let inputPrefill3 = "";
+        switch (urlArgs.editType) {
             case "person":
                 inputLabel1 = "First Name";
                 inputLabel2 = "Last Name";
                 inputLabel3 = "Image";
+                if (urlArgs.editMode == true) { // TODO: 'val == true' is bad. Fix this later by updating router to handle types
+                    inputPrefill1 = "edit";
+                    inputPrefill2 = "";
+                    inputPrefill3 = "";
+                }
+                document.getElementById("done-button").onclick = () => { DataLoader.; };
                 break;
             case "topic":
                 inputLabel1 = "Title";
                 inputLabel2 = "Description";
                 inputLabel3 = "Image";
+                if (urlArgs.editMode == true) {
+                    inputPrefill1 = "edit";
+                    inputPrefill2 = "";
+                    inputPrefill3 = "";
+                }
+                document.getElementById("done-button").onclick = () => { };
                 break;
             default:
                 break;
@@ -31,19 +46,15 @@ class HtmlLoader {
             inputs[1].children[0].innerHTML = inputLabel2;
             inputs[2].children[0].innerHTML = inputLabel3;
             //Updates inputs
-            inputs[0].children[1].value = "oi";
-            inputs[1].children[1].value = "oi";
-            inputs[2].children[1].value = "oi";
+            inputs[0].children[1].value = inputPrefill1;
+            inputs[1].children[1].value = inputPrefill2;
+            inputs[2].children[1].value = inputPrefill3;
         }
     }
 }
 class DataLoader {
-    loadPeople() {
+    static loadData(object, id) {
     }
-    loadTopics() {
-    }
-    saveTopics() {
-    }
-    savePeople() {
+    static saveData(object) {
     }
 }
--- scripts/SPA-Tools/loader.ts
+++ scripts/SPA-Tools/loader.ts
@@ -9,7 +9,9 @@ class HtmlLoader {
 
     static loadListEditor() {
         interface UrlArgsEditor{
-            editMode : string;
+            editType : string;
+            editMode : boolean;
+            editRef  : number;
         }
 
         let urlArgs = (SpaRouter.getUrlArgs() as UrlArgsEditor)
@@ -17,18 +19,38 @@ class HtmlLoader {
         let inputLabel1 = "Error!";
         let inputLabel2 = "Error!";
         let inputLabel3 = "Error!";
+        
+        let inputPrefill1 = "";
+        let inputPrefill2 = "";
+        let inputPrefill3 = "";
 
-        switch (urlArgs.editMode) {
+        switch (urlArgs.editType) {
             case "person":
                 inputLabel1 = "First Name";
                 inputLabel2 = "Last Name";
                 inputLabel3 = "Image";    
+
+                if (urlArgs.editMode == true) { // TODO: 'val == true' is bad. Fix this later by updating router to handle types
+                    inputPrefill1 = "edit";
+                    inputPrefill2 = "";
+                    inputPrefill3 = "";
+                }
+
+                (document.getElementById("done-button") as HTMLLinkElement).onclick = () => {DataLoader.}
                 break;
                 
             case "topic":
                 inputLabel1 = "Title";
                 inputLabel2 = "Description";
                 inputLabel3 = "Image";
+
+                if (urlArgs.editMode == true) {
+                    inputPrefill1 = "edit";
+                    inputPrefill2 = "";
+                    inputPrefill3 = "";
+                }
+
+                (document.getElementById("done-button") as HTMLLinkElement).onclick = () => {}
                 break;
     
             default:
@@ -44,27 +66,19 @@ class HtmlLoader {
             inputs[2].children[0].innerHTML = inputLabel3;
     
             //Updates inputs
-            (inputs[0].children[1] as HTMLInputElement).value = "oi";
-            (inputs[1].children[1] as HTMLInputElement).value = "oi";
-            (inputs[2].children[1] as HTMLInputElement).value = "oi" ;   
+            (inputs[0].children[1] as HTMLInputElement).value = inputPrefill1;
+            (inputs[1].children[1] as HTMLInputElement).value = inputPrefill2;
+            (inputs[2].children[1] as HTMLInputElement).value = inputPrefill3;   
         }
     }
 }
 
 class DataLoader {
-    loadPeople() {
-
-    }
-
-    loadTopics() {
+    static loadData(object:string, id:number) {
         
     }
 
-    saveTopics() {
-
-    }
-
-    savePeople() {
+    static saveData(object:string) {
 
     }
 }
